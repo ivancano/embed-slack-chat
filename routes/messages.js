58c8a7d659959d43cce02d785c5b315de8edbc96
ivan@qroom.biz <ivan@qroom.biz>
2020-07-27 17:17:57 -0300

    test

--- routes/messages.js
+++ routes/messages.js
@@ -47,6 +47,7 @@ router.post('/post-file', async function(req, res, next) {
 	// An access token (from your Slack app or custom integration - xoxp, xoxb)
 	//const token = 'xoxp-1282905512672-1259065033858-1259079880194-2c142c0b9af14fcedbb6173fcb26e56d';
 	const token = req.body.token;
+	console.log(req.body);
 
 	const web = new WebClient(token);
 	const conversationId = req.body.conversationId;
