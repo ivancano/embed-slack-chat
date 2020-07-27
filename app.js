e0102d25a33f523c4f6b362375d9033400e80aa7
ivan@qroom.biz <ivan@qroom.biz>
2020-07-27 20:00:26 -0300

    test

--- app.js
+++ app.js
@@ -26,16 +26,14 @@ const corsOptions = {
   }
 }
 app.use(cors(corsOptions))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
