9d1844bd453dd8791ba64d363ca99268e3145517
ivan@qroom.biz <ivan@qroom.biz>
2020-07-30 10:29:07 -0300

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   apps : [{
-    name: 'Embed-Slack'
-    script: 'app.js',
+    name: 'Embed-Slack',
+    script: './app.js',
     env: {
+      NODE_ENV: 'development',
+      port: 3006;
+    },
+    env_production: {
       NODE_ENV: 'production',
       port: 3006;
     }
